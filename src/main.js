4110d389c7bdb06cb11c5d5f6b92dcd87e9b5c49
Teresa Campbell <teresa.j.campbell@gmail.com>
2022-05-02 20:15:50 -0400

    initial commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+
+
+function getNotes()
+ {
+     
+ }
+
+function saveNotes(notes){
+
+
+}
+
+function createNoteElement(id, content){
+
+}
+
+function addNote() {
+
+}
+
+function updateNote(id, newContent) {
+
+}
+
+function deleteNote(id, element) {
+
+}
